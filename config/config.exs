65ca77229048826d45ffd97cb57b07ac233a1a75
Andreas Kasprzok <akasprzok@gmail.com>
2022-06-02 22:22:32 -0700

    more basics

--- config/config.exs
+++ config/config.exs
@@ -7,11 +7,7 @@ if Mix.env() != :prod do
     hooks: [
       pre_commit: [
         tasks: [
-          {:mix_task, :format}
-        ]
-      ],
-      pre_push: [
-        tasks: [
+          {:mix_task, :format},
           {:cmd, "mix compile --warnings-as-errors"},
           {:mix_task, :credo, ["--strict"]},
           {:mix_task, :test}
